8bb3d1058fa7cd450f65e2b843326e44dddc5682
Pacific99 <66220164+Pacific99@users.noreply.github.com>
2021-11-20 13:19:17 +0530

    Video section JavaScript

--- js/main.js
+++ js/main.js
@@ -1,95 +1,107 @@
+$(document).ready(function() {
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
+
+
+
 $(function() {
 
-    'use strict';
+  'use strict';
 
-    $('.fakeLoader').fakeLoader({
+  $('.fakeLoader').fakeLoader({
 
-        timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
+    timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
 
-        zIndex: "999",//Default zIndex
+    zIndex: "999", //Default zIndex
 
-        spinner: "spinner3",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+    spinner: "spinner3", //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
 
-        bgColor: "#212121" //Hex, RGB or RGBA colors
+    bgColor: "#212121" //Hex, RGB or RGBA colors
 
+  });
+
+  // smooth scroll
+  $("a").on("click", function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate({
+
+        scrollTop: $(hash).offset().top - 50
+
+      }, 850);
+
+    }
+
+  });
+
+  // hide navbar on mobile after click
+  $('.navbar-nav a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // carousel resume
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    margin: 10
+  });
+
+  // collapse show on resume
+  $('.collapse-show').collapse();
+
+  // porfolio filterizr
+  $('.filtr-container').imagesLoaded(function() {
+    var filterizr = $('.filtr-container').filterizr();
+  });
+
+  // portfolio filter
+  $('.portfolio-filter-menu li').on('click', function() {
+    $('.portfolio-filter-menu li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  // portfolio magnific popup
+  $('.portfolio').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: '.portfolio-popup', // the selector for portfolio item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
     });
+  });
 
-    // smooth scroll
-    $("a").on("click", function(event) {
+  // navbar on scroll
+  $(window).on("scroll", function() {
 
-        if (this.hash !== "") {
-            event.preventDefault();
+    var vScroll = $(this).scrollTop();
 
-            var hash = this.hash;
+    if (vScroll > 100) {
+      $(".navbar").addClass("fix");
+    } else {
+      $(".navbar").removeClass("fix");
+    }
 
-            $("html, body").animate({
+  });
 
-                scrollTop: $(hash).offset().top - 50
-
-            }, 850);
-
-        }
-
-    });
-
-    // hide navbar on mobile after click
-    $('.navbar-nav a').on('click', function() {
-        $('.navbar-collapse').collapse('hide');
-    });
-
-    // carousel resume
-    $('.owl-carousel').owlCarousel({
-        items: 1,
-        margin: 10
-    });
-
-    // collapse show on resume
-    $('.collapse-show').collapse();
-
-    // porfolio filterizr
-    $('.filtr-container').imagesLoaded( function() {
-        var filterizr = $('.filtr-container').filterizr();
-    });
-
-    // portfolio filter
-    $('.portfolio-filter-menu li').on('click', function() {
-        $('.portfolio-filter-menu li').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    // portfolio magnific popup
-    $('.portfolio').each(function() { // the containers for all your galleries
-        $(this).magnificPopup({
-            delegate: '.portfolio-popup', // the selector for portfolio item
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    });
-
-    // navbar on scroll
-    $(window).on("scroll", function() {
-
-        var vScroll = $(this).scrollTop();
-
-        if( vScroll > 100) {
-            $(".navbar").addClass("fix");
-        }
-        else {
-            $(".navbar").removeClass("fix");
-        }
-
-    });
-
-    // wrap code in IIFE to keep it from global scope
-    let links = document.querySelectorAll('a'); // grab all <a> tags that trigger the image gallery
-    let imageContainer = document.querySelector('.image-container'); // grab the div that will contain the images
-    let imagesCollection = document.querySelectorAll('.image-container img');
+  // wrap code in IIFE to keep it from global scope
+  let links = document.querySelectorAll('a'); // grab all <a> tags that trigger the image gallery
+  let imageContainer = document.querySelector('.image-container'); // grab the div that will contain the images
+  let imagesCollection = document.querySelectorAll('.image-container img');
 
   function displayImage(imgs, album) { // function to check the data-album attr and check against the button that was clicked
     imgs.forEach((image) => {
-      if(image.dataset.album == album) {
+      if (image.dataset.album == album) {
         image.classList.remove('hide');
       } else {
         image.classList.add('hide');
@@ -97,42 +109,50 @@ $(function() {
     });
   }
 
+  document.querySelector('.two').style.display = 'none';
+  document.querySelector('.three').style.display = 'none';
 
-    links.forEach((link) => { // loop through <a> tags and create click event on each
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
 
-            switch (link.textContent) { // check name of link clicked
-                case "Marketing & Branding": // link 1 text
+  links.forEach((link) => { // loop through <a> tags and create click event on each
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
 
-                    displayImage(imagesCollection, 'Marketing & Branding'); //display images from album 1
-                    document.querySelector('.one').style.display = '';
+      switch (link.textContent) { // check name of link clicked
+        case "Marketing & Branding": // link 1 text
+          document.querySelector('.one').style.display = 'flex';
+          displayImage(imagesCollection, 'Marketing & Branding'); //display images from album 1
+          // document.querySelector('.one').style.display = '';
 
-                    break;
-                case "Social Media": // link 2 text
-                      displayImage(imagesCollection, 'Social Media'); //display images from album 2
-                      document.querySelector('.one').style.display = 'none';
-                    break;
-                case "album 3": // link 3 text
+          break;
+        case "Social Media": // link 2 text
+          document.querySelector('.one').style.display = 'none';
+          document.querySelector('.two').style.display = 'flex';
+          displayImage(imagesCollection, 'Social Media'); //display images from album 2
 
-                    displayImage(imagesCollection, 'album 3'); //display images from album 3
+          break;
+        case "Videos": // link 3 text
+        document.querySelector('.one').style.display = 'none';
+          document.querySelector('.two').style.display = 'none';
+          document.querySelector('.three').style.display = 'flex';
+          displayImage(imagesCollection, 'Videos'); //display images from album 3
 
-                    break;
 
-                case "All": // // link 4 text - display all images at once
+          break;
 
-                        imagesCollection.forEach((image) => {
-                          image.classList.remove('none');
-                          image.classList.remove('');
-                          image.classList.remove('hide');
+        case "All": // // link 4 text - display all images at once
 
-                   });
+          imagesCollection.forEach((image) => {
+            // image.classList.remove('none');
+            // image.classList.remove('');
+            image.classList.remove('hide');
 
-                    break;
-            }
+          });
 
-        });
+          break;
+      }
+
     });
+  });
 
 
 });
